6d3bb8a1393914a0d9c495c580bd8b0dfad18ac2
charlesarce <carceguet@gmail.com>
2022-11-28 02:31:22 -0300

    clase03 completada

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const btn = document.querySelector("[data-form-btn]")
-const input = document.querySelector("[data-form-input]")
+
 
 const createTask = (evento) => {
     evento.preventDefault()
-    const input = document.querySelector("[data-form-input]")
-    console.log(input.value)
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li")
+    task.classList.add("card")
+    //backticks
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+   
+    task.innerHTML= content
+
+    list.appendChild(task)
+    console.log(content);
 };
 
 btn.addEventListener("click", createTask)
